f8ed5ebe314246989cf1f7029abfa63def94afcd
Dog2puppy <colecooper2005@icloud.com>
2018-08-12 09:31:46 -0400

    Don't use player name for key

--- ExampleScript1.lua
+++ ExampleScript1.lua
@@ -1,7 +1,7 @@
 local manager = require(game.ReplicatedStorage:WaitForChild("LuaCoinManager"))
 
 game.Players.PlayerAdded:connect(function(plr)
-	local key = plr.Name.."_"..plr.userId
+	local key = plr.userId
 	manager:SetupPlayer("MasterBank",key)
 	manager:GetInformation("MasterBank",key)
 end)
